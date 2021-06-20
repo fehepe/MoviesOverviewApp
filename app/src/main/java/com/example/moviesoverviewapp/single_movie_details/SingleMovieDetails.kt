956af6bb626ad1498b4cf5ef36eb492b71b0bcce
fehepe <40445768+fehepe@users.noreply.github.com>
2021-06-20 13:50:50 -0400

    Sigle Movie Detail  Simple Design completed

---
+++ app/src/main/java/com/example/moviesoverviewapp/single_movie_details/SingleMovieDetails.kt
@@ -0,0 +1,12 @@
+package com.example.moviesoverviewapp.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesoverviewapp.R
+
+class SingleMovieDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie_details)
+    }
+}
